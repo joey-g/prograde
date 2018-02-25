051b83af29f38d2c8478b314c6e22cd02fd82454
joey-g <joey@boomtownroi.com>
2018-02-25 15:34:11 -0500

    Initial pass at converting theme to React.

--- site/src/App.js
+++ site/src/App.js
@@ -1,18 +1,211 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="body homepage">
+        <div id="page-wrapper">
+          {/* Header */}
+          <div id="header-wrapper" className="wrapper">
+            <div id="header">
+
+              {/* Logo */}
+              <div id="logo">
+                <h1><a href="index.html">Joey Gryder</a></h1>
+                <p>{'Sr. Software Engineer in Test - Charleston, SC'}</p>
+              </div>
+
+              {/* Nav */}
+              <nav id="nav">
+                <ul>
+                  <li className="current"><a href="index.html">Home</a></li>
+                  <li><a href="#highlights">Portfolio</a></li>
+                  <li><a href="#footer">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          {/* End Header */}
+
+          {/* Intro */}
+          <div id="intro-wrapper" className="wrapper style1">
+            <div className="title">The Introduction</div>
+    				<section id="intro" className="container">
+    					<p className="style1">So in case you were wondering what this is all about ...</p>
+    					<p className="style2">
+                Escape Velocity is a free responsive<br className="mobile-hide" /> site template by <a href="http://html5up.net" className="nobr">HTML5 UP</a>
+    					</p>
+    					<p className="style3">
+                {"It's "}<strong>responsive</strong>{", built on "}<strong>HTML5</strong>{" and "}<strong>CSS3</strong>
+                {" and released for free under the "}<a href="http://html5up.net/license"> Creative Commons Attribution 3.0 license</a>
+                {" so use it for any of your personal or commercial projects -- just be sure to credit us!"}
+              </p>
+    					<ul className="actions">
+    						<li><a href="#" className="button style3 big">Proceed</a></li>
+    					</ul>
+            </section>
+          </div>
+          {/* End Intro */}
+
+          {/* Main */}
+    			<div className="wrapper style2">
+    				<div className="title">The Details</div>
+    				<div id="main" className="container">
+
+    					{/* Image */}
+    					<a href="#" className="image featured">
+                <img src="images/pic01.jpg" alt="" />
+              </a>
+
+    					{/* Features */}
+    					<section id="features">
+                <header className="style1">
+    							<h2>Dolor consequat feugiat amet veroeros</h2>
+    							<p>Feugiat dolor nullam orci pretium phasellus justo</p>
+    						</header>
+                {/* Feature List */}
+    						<div className="feature-list">
+    							<div className="row">
+    								<div className="6u 12u(mobile)">
+    									<section>
+    										<h3 className="icon fa-comment">Mattis velit diam vulputate</h3>
+    										<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
+    									</section>
+    								</div>
+    								<div className="6u 12u(mobile)">
+    									<section>
+    										<h3 className="icon fa-refresh">Lorem ipsum dolor sit veroeros</h3>
+    										<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
+    									</section>
+    								</div>
+    							</div>
+    							<div className="row">
+    								<div className="6u 12u(mobile)">
+    									<section>
+    										<h3 className="icon fa-picture-o">Pretium phasellus justo lorem</h3>
+    										<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
+    									</section>
+    								</div>
+    								<div className="6u 12u(mobile)">
+    									<section>
+    										<h3 className="icon fa-cog">Tempus sed pretium orci</h3>
+    										<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
+    									</section>
+    								</div>
+    							</div>
+    							<div className="row">
+    								<div className="6u 12u(mobile)">
+    									<section>
+    										<h3 className="icon fa-wrench">Aliquam consequat et feugiat</h3>
+    										<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
+    									</section>
+    								</div>
+    								<div className="6u 12u(mobile)">
+    									<section>
+    										<h3 className="icon fa-check">Dolore laoreet aliquam mattis</h3>
+    										<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
+    									</section>
+    								</div>
+    							</div>
+    						</div>
+                {/* End Feature List */}
+    						<ul className="actions actions-centered">
+    							<li><a href="#" className="button style1 big">Get Started</a></li>
+    							<li><a href="#" className="button style2 big">More Info</a></li>
+    						</ul>
+    					</section>
+              {/* End Features */}
+
+    				</div>
+    			</div>
+          {/* End Main */}
+
+          {/* Footer */}
+    			<div id="footer-wrapper" className="wrapper">
+            <div className="title">The Rest Of It</div>
+    				<div id="footer" className="container">
+    					<header className="style1">
+    						<h2>Get In Touch</h2>
+    						<p>
+    							Sed turpis tortor, tincidunt sed ornare in metus porttitor mollis nunc in aliquet.<br /> Nam pharetra laoreet imperdiet volutpat etiam consequat feugiat.
+    						</p>
+              </header>
+
+              <hr />
+    					<div className="row 150%">
+                <div className="6u 12u(mobile)">
+
+                  {/* Contact Form */}
+                  <section>
+                  	<form method="post" action="#">
+                  		<div className="row 50%">
+                  			<div className="6u 12u(mobile)">
+                  				<input type="text" name="name" id="contact-name" placeholder="Name" />
+                  			</div>
+                  			<div className="6u 12u(mobile)">
+                  				<input type="text" name="email" id="contact-email" placeholder="Email" />
+                  			</div>
+                  		</div>
+                  		<div className="row 50%">
+                  			<div className="12u">
+                  				<textarea name="message" id="contact-message" placeholder="Message" rows="4"></textarea>
+                  			</div>
+                  		</div>
+                  		<div className="row">
+                  			<div className="12u">
+                  				<ul className="actions">
+                  					<li><input type="submit" className="style1" value="Send" /></li>
+                  					<li><input type="reset" className="style2" value="Reset" /></li>
+                  				</ul>
+                  			</div>
+                  		</div>
+                  	</form>
+                  </section>
+                  {/* End Contact Form */}
+
+                </div>
+    						<div className="6u 12u(mobile)">
+
+    							{/* Contact Info */}
+    							<section className="feature-list small">
+    								<div className="row">
+    									<div className="6u 12u(mobile)">
+    										<section>
+    											<h3 className="icon fa-envelope">Email</h3>
+    											<p>
+    												<a href="mailto:joey.gryder@gmail.com">joey.gryder@gmail.com</a>
+    											</p>
+    										</section>
+    									</div>
+    									<div className="6u 12u(mobile)">
+    										<section>
+    											<h3 className="icon fa-comment">Social</h3>
+    											<p>
+    												<a href="#">@untitled-corp</a><br />
+    												<a href="#">linkedin.com/untitled</a><br />
+    												<a href="#">facebook.com/untitled</a>
+    											</p>
+    										</section>
+    									</div>
+    								</div>
+    							</section>
+                  {/* End Contact Info */}
+
+    						</div>
+              </div>
+
+              <hr />
+            </div>
+            <div id="copyright">
+    					<ul>
+    						<li>Joey Gryder &copy; {(new Date()).getFullYear()}</li>
+    						<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+    					</ul>
+            </div>
+    			</div>
+          {/* End Footer */}
+        </div>
       </div>
     );
   }

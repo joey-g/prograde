596aded87150d415654ed87dff0ecafc70ae745b
joey-g <joey@boomtownroi.com>
2018-03-22 22:45:11 -0400

    Wrapped app component in react-ga withTracker().

--- site/src/index.js
+++ site/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import ScrollToTop from './ScrollToTop';
 import registerServiceWorker from './registerServiceWorker';
+import withTracker from './withTracker';
 
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
@@ -12,7 +13,7 @@ import './index.css';
 ReactDOM.render((
     <BrowserRouter>
         <ScrollToTop>
-            <App />
+            <Route component={withTracker(App)}/>
         </ScrollToTop>
     </BrowserRouter>),
     document.getElementById('root')

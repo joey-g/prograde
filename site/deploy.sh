25e2d4709588806fc8b19babc2299e6befa43d49
joey-g <joey@boomtownroi.com>
2018-04-19 23:07:24 -0400

    Added hash bang to top of deploy script.

--- site/deploy.sh
+++ site/deploy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 aws s3 rm s3://www.joeygryder.com --recursive
 aws s3 sync build s3://www.joeygryder.com
 aws cloudfront create-invalidation --distribution-id E3EB2IVJAI21K0 --paths '/*'

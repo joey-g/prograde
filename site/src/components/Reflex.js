cf3f6a42f225fae56203f80cb0817c8b359da141
joey-g <joey@boomtownroi.com>
2018-04-24 22:30:21 -0400

    Continued writing copy for Reflex component.

--- site/src/components/Reflex.js
+++ site/src/components/Reflex.js
@@ -47,19 +47,49 @@ class Reflex extends Component {
                                         <img src={ReflexRunDetailsImg} alt="testing-framework-details" />
                                     </div>
                                     <p>
-                                        Before I joined &nbsp;<a href="https://boomtownroi.com/">BoomTown ROI</a> as a Sr. SET, 
-                                        the team had historically built one-off testing scripts that were executed manually 
-                                        before a release. These scripts were often built in disparate languages and testing 
-                                        frameworks, with no standardized process for execution, storage, or reporting of 
-                                        testing results.
+                                        Before I joined &nbsp;<a href="https://boomtownroi.com/">BoomTown ROI</a> as a Sr. 
+                                        Software Engineer in Test, the team had historically built one-off testing scripts 
+                                        that were executed manually before a release. These scripts were often built in 
+                                        disparate languages and testing frameworks, with no standardized process for 
+                                        execution, storage, or reporting of testing results.
                                     </p>
                                     <p>
                                         After joining the team, I implemented a testing framework that utilizes Cucumber as its
                                         core execution and reporting engine. By standardizing all tests around Cucumber, this 
-                                        provided a common language for the team to use when communicating ideas to any stakeholders 
-                                        on a given project. This also provided a common, repeatable workflow for testers to use when 
-                                        going from building a test locally, through to running their new tests against the full 
-                                        framework.
+                                        provided a common language for the team to use when communicating ideas to stakeholders 
+                                        on a given project. This also provided a repeatable workflow for testers to use when 
+                                        going from building a test locally to running their new tests against the full framework. 
+                                    </p>
+                                    <p>
+                                        The framework is built upon the following technologies: 
+                                    </p>
+                                    <h4>RESTful Web Service Built in Sinatra</h4>
+                                    <p>
+                                        Sitting at the core of the framework is a &nbsp;
+                                        <a href="https://github.com/sinatra/sinatra">Sinatra</a>-based RESTful web service
+                                        responsible for handling all requests to start new test jobs, retrieve detailed results 
+                                        for previous jobs, and provide CRUD operations on various test configuration tables. The
+                                        service is deployed as a Docker container to Amazon's ECS.
+                                    </p>
+                                    <h4>MySQL and DynamoDB Datastores</h4>
+                                    <p>
+                                        For the storage of all historical test runs, we utilize a MySQL database hosted in
+                                        Amazon RDS. For non-relational data, such as the details of a currently executing test
+                                        job, we utilize Amazon's DynamoDB.
+                                    </p>
+                                    <h4>Containerized Cucumber Job Runners</h4>
+                                    <p>
+                                        The actual execution of tests through the Cucumber binary is handled with a 
+                                        batch-processing architecture to improve test parallelization and decrease total test
+                                        run times. Each test runner is built as a small Ruby application and is deployed in a 
+                                        Docker container to Amazon ECS. These test runners then poll a queue for Cucumber jobs 
+                                        to execute.
+                                    </p>
+                                    <h4>Front-end Dashboard Built in React</h4>
+                                    <p>
+                                        To provide an interface for users to start new tests and investigate the results of
+                                        previous runs, I implemented a dashboard in React that sits atop the Sinatra web
+                                        service. The dashboard is hosted as a static site out of Amazon S3.
                                     </p>
                                 </article>
                                 <div className="row 150%">
